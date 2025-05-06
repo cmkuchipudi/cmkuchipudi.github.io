501ca50c064878cb84a151e7a1936476ebd3a274
srirampingali <sriram26354@gmail.com>
2025-05-06 17:49:20 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,6 @@
+alert("Happy Birthday Sinnuu!");
+alert("I love how you passionately talk about your dance classes, and how you are always excited to teach your art to as many people as possible.");
+alert("Well, to do that, I think you should build a nice portfolio website to showcase your work. And inspire others.");
+alert("That's my gift to you.");
+alert("While this page is still a work in progress, I hope it motivates you enough to start building your own franchise in the future.");
+alert("I hope you like it!");
